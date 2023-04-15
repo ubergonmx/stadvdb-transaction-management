e9db64710d29468199bf6d0aa90b4db345e9af3e
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-04-15 18:12:26 +0800

    fix `connectDB()`

--- model/db.js
+++ model/db.js
@@ -3,41 +3,30 @@ const nodeEnv = process.env.NODE_ENV;
 const nodeNumber = process.env.NODE_NUMBER;
 
 const node1 = mysql.createPool({
-        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE1_DB_HOST,
-        port: (nodeEnv==='development') ? process.env.NODE1_DB_PORT : process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.NODE1_DB_NAME,
+    host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE1_DB_HOST,
+    port: (nodeEnv==='development') ? process.env.NODE1_DB_PORT : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.NODE1_DB_NAME,
 });
 
 const node2 = mysql.createPool({
-        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE2_DB_HOST,
-        port: (nodeEnv==='development') ? process.env.NODE2_DB_PORT : process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.NODE2_DB_NAME,
+    host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE2_DB_HOST,
+    port: (nodeEnv==='development') ? process.env.NODE2_DB_PORT : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.NODE2_DB_NAME,
 });
 
 const node3 = mysql.createPool({
-        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE3_DB_HOST,
-        port: (nodeEnv==='development') ? process.env.NODE3_DB_PORT : process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.NODE3_DB_NAME,
+    host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE3_DB_HOST,
+    port: (nodeEnv==='development') ? process.env.NODE3_DB_PORT : process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.NODE3_DB_NAME,
 });
 
 function connectDB(){
-    if(nodeEnv === 'production') {
-        local.getConnection((err) => {
-            if (err) {
-                console.log(`Error connecting to ${dbName} database`)
-                console.log(err);
-            } else {
-                console.log(`Connected to ${dbName} database`);
-            }
-        });
-    }
-
     if(node1) {
         node1.getConnection((err) => {
             if (err) {
