85bb827ded38ac642e45d0269b9239ef94c46c78
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-04-15 17:58:30 +0800

    change node db connections

--- model/db.js
+++ model/db.js
@@ -24,10 +24,10 @@ switch (nodeNumber) {
         break;
 }
 
-// create pool for local database
+// for Proxmox VM local database
 const local = mysql.createPool({
     host: process.env.LOCAL_DB_HOST,
-    port: process.env.LOCAL_PORT,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: dbName,
@@ -35,8 +35,8 @@ const local = mysql.createPool({
 
 function setNode1() {
     node1 = mysql.createPool({
-        host: process.env.NODE_HOST,
-        port: process.env.NODE1_PORT,
+        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE1_DB_HOST,
+        port: (nodeEnv==='development') ? process.env.NODE1_DB_PORT : process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.NODE1_DB_NAME,
@@ -45,8 +45,8 @@ function setNode1() {
 
 function setNode2() {
     node2 = mysql.createPool({
-        host: process.env.NODE_HOST,
-        port: process.env.NODE2_PORT,
+        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE2_DB_HOST,
+        port: (nodeEnv==='development') ? process.env.NODE2_DB_PORT : process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.NODE2_DB_NAME,
@@ -55,8 +55,8 @@ function setNode2() {
 
 function setNode3() {
     node3 = mysql.createPool({
-        host: process.env.NODE_HOST,
-        port: process.env.NODE3_PORT,
+        host: (nodeEnv==='development') ? process.env.NODE_DB_HOST : process.env.NODE3_DB_HOST,
+        port: (nodeEnv==='development') ? process.env.NODE3_DB_PORT : process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.NODE3_DB_NAME,
