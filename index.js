eaac73ab08c62411a9e5eea1ec9ff2a5beccb61a
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-04-16 12:03:43 +0800

    add supertest and tests

--- index.js
+++ index.js
@@ -50,3 +50,5 @@ setInterval(() => {
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+module.exports = app;
--- test/concurrency.test.js
+++ test/concurrency.test.js
@@ -1,26 +1,52 @@
 require('dotenv').config();
+
 const mysql = require('mysql2');
 
-const nodeEnv = process.env.NODE_ENV;
+// const request = require('supertest');
+
+const app = require('../routes/routes');
+
+describe('Perform Basic Api Requests', () => {
+  test('should be able to connect to the server', async () => {
+    expect(app).toBeDefined();
+  });
+  // test('should be able to ping the server', async () => {
+  //   const res = await request(app).get('/');
+  //   expect(res.statusCode).toEqual(200);
+  // });
+});
 
 describe('Perform Basic Queries', () => {
   let connection;
 
   beforeEach(async () => {
-    connection = await mysql.createConnection({
-      host: nodeEnv === 'development' ? process.env.NODE_DB_HOST : process.env.NODE1_DB_HOST,
-      port: nodeEnv === 'development' ? process.env.NODE1_DB_PORT : process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.NODE1_DB_NAME,
-    });
+    jest.useFakeTimers();
+    connection = mysql
+      .createPool({
+        host: process.env.NODE_DB_HOST,
+        port: process.env.NODE1_DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.NODE1_DB_NAME,
+      })
+      .promise();
+
+    await connection.getConnection();
     console.log('Connection created');
   });
-  it('should be able to connect to the database', async () => {
+
+  afterEach(async () => {
+    await connection.end();
+    console.log('Connection ended');
+  });
+
+  test('should be able to connect to the database', async () => {
     expect(connection).toBeDefined();
   });
-  it('should be able to ping the database', async () => {
-    const [rows] = await connection.execute('SELECT 1');
-    expect(rows).toBeDefined();
-  });
+  // test('should be able to ping the database', async () => {
+  //   const [rows, fields] = await connection.query('SELECT 1;');
+  //   console.log(rows);
+  //   console.log(fields);
+  //   expect(rows).toBeDefined();
+  // }, 10000);
 });
