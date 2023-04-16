51e99d13699d43d2c5909ec8121165d381925f04
aaron <aaron_palpallatoc@dlsu.edu.ph>
2023-04-16 18:35:03 +0800

    fix getAllMovies

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -17,51 +17,107 @@ const adminController = {
   },
   getAllMovies: (req, res) => {
     let movies;
-    if (db.ping(db.node1())) {
-      db.node1().query('SELECT * FROM movies', (err, result) => {
-        if (err) {
-          console.log(err);
-          res.send(err);
-        } else {
-          res.send(result);
-        }
-      });
-    } else if (db.ping(db.localNode())) {
-      db.localNode().query('SELECT * FROM movies', (err, result) => {
-        movies = result;
-        if (err) {
-          console.log(err);
-          res.send(err);
-        } else {
-          if (process.env.NODE_NUMBER === '2') {
-            if (db.ping(db.node3())) {
-              db.node3().query('SELECT * FROM movies', (errNode, node3) => {
-                if (errNode) {
-                  console.log(errNode);
-                  res.send(errNode);
-                } else {
-                  movies = movies.concat(node3).sort((a, b) => a.id - b.id);
-                }
-              });
-            }
+    db.ping(db.node1(), (isNode1Up) => {
+      if (isNode1Up) {
+        db.node1().query('SELECT * FROM movies', (err, result) => {
+          if (err) {
+            console.log(err);
+            res.send(err);
+          } else {
+            res.send(result);
           }
-          if (process.env.NODE_NUMBER === '3') {
-            if (db.ping(db.node2())) {
-              db.query('SELECT * FROM movies', (errNode, node2) => {
-                if (errNode) {
-                  console.log(errNode);
-                  res.send(errNode);
-                } else {
-                  movies = movies.concat(node2).sort((a, b) => a.id - b.id);
+        });
+      } else {
+        db.ping(db.localNode(), (isLocalNodeUp) => {
+          if (isLocalNodeUp) {
+            db.localNode().query('SELECT * FROM movies', (err, result) => {
+              movies = result;
+              if (err) {
+                console.log(err);
+                res.send(err);
+              } else {
+                if (process.env.NODE_NUMBER === '2') {
+                  db.ping(db.node3(), (isNode3Up) => {
+                    if (isNode3Up) {
+                      db.node3().query('SELECT * FROM movies', (errNode, node3) => {
+                        if (errNode) {
+                          console.log(errNode);
+                          res.send(errNode);
+                        } else {
+                          movies = movies.concat(node3).sort((a, b) => a.id - b.id);
+                        }
+                      });
+                    }
+                  });
                 }
-              });
-            }
+                if (process.env.NODE_NUMBER === '3') {
+                  db.ping(db.node2(), (isNode2Up) => {
+                    if (isNode2Up) {
+                      db.query('SELECT * FROM movies', (errNode, node2) => {
+                        if (errNode) {
+                          console.log(errNode);
+                          res.send(errNode);
+                        } else {
+                          movies = movies.concat(node2).sort((a, b) => a.id - b.id);
+                        }
+                      });
+                    }
+                  });
+                }
+              }
+            });
+          } else {
+            res.send('No database connection available. Please try again later.');
           }
-        }
-      });
-    } else {
-      res.send('No database connection');
-    }
+        });
+      }
+    });
+
+    // if (db.ping(db.node1())) {
+    //   db.node1().query('SELECT * FROM movies', (err, result) => {
+    //     if (err) {
+    //       console.log(err);
+    //       res.send(err);
+    //     } else {
+    //       res.send(result);
+    //     }
+    //   });
+    // } else if (db.ping(db.localNode())) {
+    //   db.localNode().query('SELECT * FROM movies', (err, result) => {
+    //     movies = result;
+    //     if (err) {
+    //       console.log(err);
+    //       res.send(err);
+    //     } else {
+    //       if (process.env.NODE_NUMBER === '2') {
+    //         if (db.ping(db.node3())) {
+    //           db.node3().query('SELECT * FROM movies', (errNode, node3) => {
+    //             if (errNode) {
+    //               console.log(errNode);
+    //               res.send(errNode);
+    //             } else {
+    //               movies = movies.concat(node3).sort((a, b) => a.id - b.id);
+    //             }
+    //           });
+    //         }
+    //       }
+    //       if (process.env.NODE_NUMBER === '3') {
+    //         if (db.ping(db.node2())) {
+    //           db.query('SELECT * FROM movies', (errNode, node2) => {
+    //             if (errNode) {
+    //               console.log(errNode);
+    //               res.send(errNode);
+    //             } else {
+    //               movies = movies.concat(node2).sort((a, b) => a.id - b.id);
+    //             }
+    //           });
+    //         }
+    //       }
+    //     }
+    //   });
+    // } else {
+    //   res.send('No database connection');
+    // }
   },
   getMovie: (req, res) => {
     db.localNode().query(`SELECT * FROM movies WHERE id = ${req.params.id}`, (err, result) => {
